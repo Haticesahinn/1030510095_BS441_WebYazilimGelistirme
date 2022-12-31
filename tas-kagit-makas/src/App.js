418871abceb46bfc340af40eb35001db4fe3c254
Haticesahinn <haticesahin98@gmail.com>
2022-12-31 16:25:33 +0300

    updated

--- tas-kagit-makas/src/App.js
+++ tas-kagit-makas/src/App.js
@@ -1,120 +1,157 @@
-import './App.css';
-import {FaHandRock, FaHandPaper, FaHandScissors, FaHandLizard, FaHandSpock} from 'react-icons/fa';
-import {useState} from "react";
+import React, { useState, useEffect } from 'react';
+import rock from './tas.png';
+import paper from './kagit.png';
+import scissors from './makas.png';
 
-const actions = {
-    rock:["scissors","lizard"],
-    paper:["rock","spock"],
-    scissors:["paper","lizard"],
-    lizard:["paper","spock"],
-    spock:["scissors","rock"],
-};
+const Game = () => {
+    const [gameMode, setGameMode] = useState(null);
+    const [player1Choice, setPlayer1Choice] = useState(null);
+    const [player2Choice, setPlayer2Choice] = useState(null);
+    const [computerChoice, setComputerChoice] = useState(null);
+    const [result, setResult] = useState(null);
 
-function randomAction(){
-    const keys= Object.keys(actions);
-    const index=Math.floor(Math.random() * keys.length);
+    const choices = ['rock', 'paper', 'scissors'];
+    const icons = { rock, paper, scissors };
 
-    return keys[index];
-}
-
-function calculateWinner(action1,action2) {
-    if(action1===action2){
-        return 0;
-    } else if(actions[action1].includes(action2)) {
-        return -1;
-    } else if(actions[action2].includes(action1)) {
-        return 1;
-    }
-    //this should never really happen.
-    return null;
-}
-
-function ActionIcon({action,...props}){
-    const icons={
-        rock:FaHandRock,
-        paper:FaHandPaper,
-        scissors:FaHandScissors,
-        lizard: FaHandLizard,
-        spock: FaHandSpock,
+    const makeComputerChoice = () => {
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        setComputerChoice(choices[randomIndex]);
     };
 
-    const Icon=icons[action];
-    return(<Icon {...props} />);
-}
+    useEffect(() => {
+        if (gameMode === 'single' && player1Choice && !computerChoice) {
+            makeComputerChoice();
+        }
+    }, [gameMode, player1Choice, computerChoice]);
 
-function Player({name="player",score=0,action="rock"}){
-    return(
-        <div className="player">
-            <div className="score">{`${name}: ${score}`}</div>
-            <div className="action">
-                {action && <ActionIcon action={action} size={60} />}
-            </div>
-        </div>
-    );
-}
+    useEffect(() => {
+        if (gameMode === 'multi' && player1Choice && player2Choice) {
+            setResult(null);
+            if (player1Choice === player2Choice) {
+                setResult('draw');
+            } else if (
+                (player1Choice === 'rock' && player2Choice === 'scissors') ||
+                (player1Choice === 'paper' && player2Choice === 'rock') ||
+                (player1Choice === 'scissors' && player2Choice === 'paper')
+            ) {
+                setResult('player1');
+            } else {
+                setResult('player2');
+            }
+        }
+    }, [gameMode, player1Choice, player2Choice]);
 
-function ActionButton({action="rock",onActionSelected}){
-    return(
-        <button className="round-btn" onClick={() => onActionSelected(action)}>
-            <ActionIcon action={action} size={20}/>
-        </button>
-    );
-}
+    useEffect(() => {
+        if (gameMode === 'single' && player1Choice && computerChoice) {
+            setResult(null);
+            if (player1Choice === computerChoice) {
+                setResult('draw');
+            } else if (
+                (player1Choice === 'rock' && computerChoice === 'scissors') ||
+                (player1Choice === 'paper' && computerChoice === 'rock') ||
+                (player1Choice === 'scissors' && computerChoice === 'paper')
+            ) {
+                setResult('win');
+            } else {
+                setResult('lose');
+            }
+        }
+    }, [gameMode, player1Choice, computerChoice]);
 
-function ShowWinner({winner=0}){
-    const text={
-        "-1":"You win!",
-        0:"It's a Tie",
-        1:"You Lose!",
+    const handleSinglePlayerClick = () => {
+        setGameMode('single');
     };
-    return(
-        <h2>{text[winner]}</h2>
-    );
-}
 
-function App() {
-    const [playerAction,setPlayerAction]=useState("");
-    const [computerAction,setComputerAction]=useState("");
+    const handleMultiPlayerClick = () => {
+        setGameMode('multi');
+    };
 
-    const [playerScore, setPlayerScore]=useState(0);
-    const [computerScore, setComputerScore]=useState(0);
-    const [winner, setWinner]=useState(0);
-
-    const onActionSelected=(selectedAction) => {
-        const newComputerAction= randomAction();
-        setPlayerAction(selectedAction);
-        setComputerAction(newComputerAction);
-
-        const newWinner= calculateWinner(selectedAction,newComputerAction);
-        setWinner(newWinner);
-        if(newWinner===-1){
-            setPlayerScore(playerScore+1);
-        } else if(newWinner===1){
-            setComputerScore(computerScore+1);
+    const handleChoiceClick = (player, choice) => {
+        if (player === 'player1') {
+            setPlayer1Choice(choice);
+        } else {
+            setPlayer2Choice(choice);
         }
     };
 
+    const handlePlayAgainClick = () => {
+        setPlayer1Choice(null);
+        setPlayer2Choice(null);
+        setComputerChoice(null);
+        setResult(null);
+    };
+
     return (
-        <div className="center">
-            <h1>Rock Paper Scissors</h1>
-            <div>
-                <div className="container">
-                    <Player name="player" score={playerScore} action={playerAction} />
-                    <Player name="computer" score={computerScore} action={computerAction}/>
-                </div>
+        <div>
+            {gameMode === null && (
                 <div>
-                    <ActionButton action="rock" onActionSelected={onActionSelected}/>
-                    <ActionButton action="paper" onActionSelected={onActionSelected}/>
-                    <ActionButton action="scissors" onActionSelected={onActionSelected}/>
-                    <ActionButton action="lizard" onActionSelected={onActionSelected}/>
-                    <ActionButton action="spock" onActionSelected={onActionSelected}/>
+                    <Button name={"Single Player"} onClick={handleSinglePlayerClick}></Button>
+                    <Button name={"Multi Player"} onClick={handleMultiPlayerClick}></Button>
                 </div>
-                <ShowWinner winner={winner} />
+            )}
+            {gameMode === 'single' && !result && (
+                <div>
+                    {choices.map(choice => (
+                        <Button
+                            key={choice}
+                            icon={icons[choice]}
+                            onClick={() => handleChoiceClick('player1', choice)}
+                        />
+                    ))}
+                </div>
+            )}
+            {gameMode === 'multi' && !result && (
 
-            </div>
+                <div>
+                    <div>Player 1 </div>
+                    {choices.map(choice => (
+                        <Button
+                            key={choice}
+                            icon={icons[choice]}
+                            onClick={() => handleChoiceClick('player1', choice)}
+                        />
+                    ))}
+                    <p>VS</p>
+                    <div>Player 2 </div>
+                    {choices.map(choice => (
+                        <Button
+                            key={choice}
+                            icon={icons[choice]}
+                            onClick={() => handleChoiceClick('player2', choice)}
+                        />
+                    ))}
+                </div>
+            )}
+            {result && (
+                <div>
+                    {gameMode === 'single' && (
+                        <div>
+                            {result === 'win' && <div>Player: {player1Choice} <br/> Computer: {computerChoice}<p>You won!</p></div>}
+                            {result === 'lose' && <div>Player: {player1Choice} <br/> Computer: {computerChoice}<p>You lost.</p></div>}
+                            {result === 'draw' && <div>Player: {player1Choice} <br/> Computer: {computerChoice}<p>It's a draw.</p></div>}
+                        </div>
+                    )}
+                    {gameMode === 'multi' && (
+                        <div>
+                            {result === 'player1' && <p>Player 1 won!</p>}
+                            {result === 'player2' && <p>Player 2 won!</p>}
+                            {result === 'draw' && <p>It's a draw.</p>}
+                        </div>
+                    )}
+                    <Button onClick={handlePlayAgainClick} name={"Play Again"} ></Button>
+                </div>
+            )}
         </div>
+    );
+};
 
-  );
-}
+const Button = ({ icon, onClick,name }) => {
+    return (
+        <button onClick={onClick}>{name}
+            <img src={icon} alt="" />
+        </button>
+    );
+};
 
-export default App;
+export default Game;
+
